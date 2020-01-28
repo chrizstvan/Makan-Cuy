3ef6aa3f2f56478b10ebcf490d2d0581f905f0e1
Christian Stevanus <vanoyt.info@gmail.com>
2020-01-28 11:26:59 +0700

    Initial Commit

---
+++ Makan Cuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Makan Cuy
+//
+//  Created by Christian Stevanus on 28/01/20.
+//  Copyright © 2020 Christian Stevanus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

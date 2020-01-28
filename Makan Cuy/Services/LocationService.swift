fab87ae6ad91674ed196c6a5e9d774426fcd269e
Christian Stevanus <vanoyt.info@gmail.com>
2020-01-28 15:13:42 +0700

    Add location manager

---
+++ Makan Cuy/Services/LocationService.swift
@@ -0,0 +1,54 @@
+//
+//  LocationService.swift
+//  Makan Cuy
+//
+//  Created by Christian Stevanus on 28/01/20.
+//  Copyright © 2020 Christian Stevanus. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+enum Result<T> {
+    case success(T)
+    case failure(Error)
+}
+
+final class LocationService: NSObject {
+    
+    private let manager: CLLocationManager
+    
+    init(manager: CLLocationManager = .init()) {
+        self.manager = manager
+        super.init()
+        manager.delegate = self
+    }
+    
+    var newLocation: ((Result<CLLocation>) -> Void)?
+    var didChangeStatus: ((Bool) -> Void)?
+    
+    var status: CLAuthorizationStatus {
+        return CLLocationManager.authorizationStatus()
+    }
+}
+
+extension LocationService: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        newLocation?(Result.failure(error))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.sorted(by: {$0.timestamp > $1.timestamp}).first {
+            newLocation?(Result.success(location))
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined, .restricted, .denied:
+            didChangeStatus?(false)
+        default:
+            didChangeStatus?(true)
+        }
+    }
+}
